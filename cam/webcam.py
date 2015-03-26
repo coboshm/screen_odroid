19524271a7cf652778bcd96ea5c86fb52d3cf707
Marc Cobos <coboshernandez@gmail.com>
2015-03-26 09:37:55 +0100

    new cascade haars

--- cam/webcam.py
+++ cam/webcam.py
@@ -2,8 +2,16 @@ import cv2
 import sys
 from time import sleep
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+nose='Nariz.xml'
+mouth='Mouth.xml'
+frontal_face='haarcascade_frontalface_alt_tree.xml'
+eyes='frontalEyes35x16.xml'
+
+
+frontal_face = cv2.CascadeClassifier(frontal_face)
+mouth = cv2.CascadeClassifier(mouth)
+nose = cv2.CascadeClassifier(nose)
+eyes = cv2.CascadeClassifier(eyes)
 
 video_capture = cv2.VideoCapture(0)
 
@@ -11,25 +19,36 @@ while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
+    faces = frontal_face.detectMultiScale(
+        frame,
+        scaleFactor=3,
+        minNeighbors=2,
         minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        maxSize=(500,500)
+    )
+
+    mouthes = mouth.detectMultiScale(
+        frame,
+        scaleFactor=3,
+        minNeighbors=2,
+        minSize=(10, 10),
+        maxSize=(200,200)
     )
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
+       # Draw a rectangle around the faces
+    for (x, y, w, h) in mouthes:
+       cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
     # Display the resulting frame
     cv2.imshow('Video', frame)
     sys.stdout.write('\r' + "Num of faces:" + str(len(faces)) + ' ' * 20)
     sys.stdout.flush() # important
-    sleep(0.05);
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
